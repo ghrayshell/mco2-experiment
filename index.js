20828be0257e13a2049ac0b1cb77c750f1b88a48
Ghrazielle Rei <ghrazielle_deramos@dlsu.edu.ph>
2024-04-11 15:40:05 +0800

    Testing testing

---
+++ index.js
@@ -0,0 +1,59 @@
+//Dependencies
+const express = require("express");
+const path = require("path");
+const dotenv = require(`dotenv`);
+const hbs = require("hbs");
+const app = express();
+const bodyParser = require(`body-parser`);
+
+const { node1, node2, node3, node_utils } = require('./model/nodes.js'); //path to nodes file
+const synchronizer = require('./model/synchronizer.js'); //path to synchronizer file
+const replicator = require('./model/replicator.js'); //path to replicator file
+const router = require('./router/router.js'); //path to router file
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//path to the templates folder
+const templatePath = path.join(__dirname,'/templates')
+
+//ifEqual on edit.hbs page
+hbs.registerHelper("ifEqual", function(a, b, options) {
+    if (a === b) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+});
+
+//Formats the queuedate in edit.hbs
+hbs.registerHelper("formatDate", function(queuedate) {
+    if (queuedate instanceof Date) {
+        const year = queuedate.getFullYear();
+        const month = String(queuedate.getMonth() + 1).padStart(2, '0');
+        const day = String(queuedate.getDate()).padStart(2, '0');
+
+        return `${year}-${month}-${day}`;
+    } else {
+        return queuedate;
+    }
+});
+
+//configure the .env variables
+dotenv.config();
+var hostname = process.env.HOSTNAME;
+var port = process.env.PORT;
+var nodenum = process.env.NODENUM;
+
+app.set("view engine", "hbs")
+app.set("views", templatePath)
+
+app.use('/', router);
+
+app.listen(port,hostname, function () {
+    console.log(`Server ` + nodenum + `: http://`+ hostname + `:` + port);
+    replicator.replicate();
+})
+
+module.exports = app;
---
+++ model/nodes.js
@@ -0,0 +1,83 @@
+const mysql = require('mysql2/promise');
+const dotenv = require(`dotenv`).config();
+
+const node1 = mysql.createPool({
+    host: '10.2.0.87',
+    user: "server0",
+    //port: 20087,
+    password: "password",
+    database: 'allPH',
+});
+
+const node2 = mysql.createPool({
+    host:  '10.2.0.88',
+    //port: 20088,
+    user: "server1",
+    password: "password",
+    database: 'luzon_appts',
+});
+
+const node3 = mysql.createPool({
+    host: '10.2.0.89',
+    //port: 20089,
+    user: "server2",
+    password: "password",
+    database: 'vismin_appts',
+});
+
+const node_utils = {
+    pingNode: async function (node) {
+        switch (node) {
+            case 1: 
+                try { 
+                    let val = await node1.query('SELECT 1 AS solution'); 
+                    console.log(val[0][0].solution);
+                    return true;
+                }
+                catch (error) { 
+                    console.log(`ERROR: SServer is unreachable. Failed to connect to Node ${node}`); 
+                    console.log(error);
+                }
+                break;
+
+            case 2: 
+                try {
+                    let val = await node2.query('SELECT 2 AS solution');
+                    console.log(val[0][0].solution);
+                    return true;
+                }
+                catch (error) { 
+                    console.log(`ERROR: SServer is unreachable. Failed to connect to Node ${node}`); 
+                    console.log(error);
+                }
+                break;
+
+            case 3: 
+                try {
+                    let val = await node3.query('SELECT 3 AS solution');
+                    console.log(val[0][0].solution);
+                    return true;
+                }
+                catch (error) { 
+                    console.log(`ERROR: SServer is unreachable. Failed to connect to Node ${node}`); 
+                    console.log(error);
+                }
+                break;
+        }
+    },
+
+    getConnection: async function(n) {
+        switch (n) {
+            case 1: return await node1.getConnection();
+            case 2: return await node2.getConnection();
+            case 3: return await node3.getConnection();
+        }
+    }
+}
+
+module.exports = {
+    node1: node1,
+    node2: node2,
+    node3: node3,
+    node_utils
+}
